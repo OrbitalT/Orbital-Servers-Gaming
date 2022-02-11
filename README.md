35bd3c3681133a06d094ff0da5a05383dda0194f
Bryce <brycelogue@gmail.com>
2022-02-11 11:32:53 -0500

    Dynamic Tables

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ The Office Orbital Gaming Server Hub
 - [x] Init
 - [x] Electron
 - [x] List servers
-- [ ] Get updated list from DB (Dynamic Table)
+- [x] Get updated list from DB (Dynamic Table)
 - [ ] Download Button for mods and configs (only button, does not autodetect game folder)
 - [ ] Server downdetector
 - [ ] Auto download to game folder
+- [ ] Launch Game from app
--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const {
     BrowserWindow,
     Menu
 } = require('electron')
+const https = require('https');
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 1200,
-        height: 510,
+        height: 600,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
@@ -19,12 +20,6 @@ function createWindow() {
     var menu = Menu.buildFromTemplate([{
         label: 'Menu',
         submenu: [{
-                label: 'CSV',
-                click() {
-                    openCSVWindow()
-                }
-            },
-            {
                 label: 'Docs',
                 click() {
                     shell.openExternal('https://github.com/OrbitalT/Lease-Gen/blob/Master/README.md')
@@ -61,3 +56,25 @@ app.whenReady().then(() => {
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
+
+// var options = {
+//   host: 'n8n.nnukez.com',
+//   path: '/webhook-test/bb4639ce-39f5-43bc-92f8-36bac19bdace',
+//   method: 'GET'
+// };
+
+// callback = function(response) {
+//   var str = '';
+
+//   //another chunk of data has been received, so append it to `str`
+//   response.on('data', function (chunk) {
+//     str += chunk;
+//   });
+
+//   //the whole response has been received, so we just print it out here
+//   response.on('end', function () {
+//     console.log(str);
+//   });
+// }
+
+// https.request(options, callback).end();
